23c0fc1a4c1935bffec10c840b0a604da4e98b6b
jainarn03 <jainarnav281@gmail.com>
2022-10-10 23:41:54 -0400

    updated

--- Game.py
+++ Game.py
@@ -1,10 +1,13 @@
+from re import IGNORECASE
 import time
 import sys
 import random
+from tkinter import W
 
 
 timea = 0.3
-timeb = 0.04
+timeb = 0
+#timeb = 0.04
 
 def word(i):
     for character in i:
@@ -12,6 +15,28 @@ def word(i):
         sys.stdout.flush()
         time.sleep(timeb)
     time.sleep(timea)
+
+def choiceMage(name):
+    print("mage")
+    mage1 = f"I see you like mages{name}, me too!"
+    mage2 = "Now before we move forward let me explain how exactly the dungeons work"
+    mage3 = "There are a total of 10 floors in this dungeon and the monsters will get harder each floor\nYou must survive all 10 floors and kill the Final Boss for the Trophy!"
+    mage4 = "As a mage you will have the following attributes:\nHealth: 75, If your health drops to 0 then you will die and have to restart that floor\nIQ: 25, This is the amount of damage each spell will be based on\nMana: 50, You have limited mana so pick your spells wisely, if you run out of mana mid fight you die\nSpecial ability called Soul drain"
+    mage5 = f"When you slect which spell you wish to use, 2 dices will be rolled\nIf the dices roll 2 - 6 then 50% of the IQ damage will be delt\nIf the dices roll a 7 - 11 then 90% of the IQ damage will be dealt\n If the dices roll a 12 then the special ability (Soul Drain) will be used and do the max damage possible"
+    mage6 = "To begin with you will recive 3 spells\n>Zap (Does the same amount of damage as your IQ attribute and takes 20 mana)\n>Fireball (Does IQ + 10 damage but takes 30 mana)\nEarthquake (Does IQ + 25 damage but takes 40 mana)"
+    mage7 = "At the end of each floor you may enter the shop and purchase better equipment using jerry coins"
+    mage8 = f"I hope you now understand how the dungeon works{name}, Now go and get that trophy!"
+
+def choiceWarrior(name):
+    print("war")
+    war1 = f"I see you like warriors{name}, good pick!"
+    war2 = "Now before we move forward let me explain how exactly the dungeons work"
+    war3 = "There are a total of 10 floors in this dungeon and the monsters will get harder each floor\nYou must survive all 10 floors and kill the Final Boss for the Trophy!"
+    war4 = "As a warrior you will have the following attributes:\nHealth: 100, If your health drops to 0 then you will die and have to restart that floor\nStrength: 35, This is the amount of damage each hit will do to the monsters\nSpecial ability called Enchanted Shuriken"
+    war5 = f"When you slect which move you wish to use, 2 dices will be rolled\nIf the dices roll 2 - 6 then you will lose 10% of your overlall health\nIf the dices roll a 7 - 11 then you lose 5% of your overall health\n If the dices roll a 12 then the special ability (Enchanted Shuriken) will be used and do the max damage possible"
+    war6 = f"To begin with you will recive 3 moves\nWeapon throw (Does the same amount of damage as your strength attribute)\nCharge (Does strength + 20 damage but you lose +5% of your overall health)\nBlood bath (Does strength + 30 damage but you lose +10% of your overall health)"
+    war7 = "At the end of each floor you may enter the shop and purchase better equipment using jerry coins"
+    war8 = f"I hope you now understand how the dungeon works{name}, Now go and get that trophy!"
 
 def intro():
     intro1 = "Hello there!\n"
@@ -26,8 +51,6 @@ def intro():
     intromage = "Mage Attributes:\nHealth: 75\nIQ: 25\nMana: 50"
     space = "\n======================================"
     introwarrior = "Warrior Attributes:\nHealth: 100\nStrength: 35"
-    introchoice = "Please pick your class (Mage OR Warrior): "
-    introname = "What would you like me to call you: "
     word(intro1)
     word(intro2)
     word(intro3)
@@ -42,31 +65,21 @@ def intro():
     print(space)
     word(introwarrior)
     print(space)
+
+def choice():
+    introchoice = "Please pick your class (Mage OR Warrior): "
+    introname = "What would you like me to call you: "
     word(introchoice)
-    choice = input()
+    choice = input("")
+    print(choice)
     word(introname)
     name = input()
-    return name, choice
-
-def pickMage(name):
-    mage1 = f"I see you like mages{name}, me too!"
-    mage2 = "Now before we move forward let me explain how exactly the dungeons work"
-    mage3 = "There are a total of 10 floors in this dungeon and the monsters will get harder each floor\nYou must survive all 10 floors and kill the Final Boss for the Trophy!"
-    mage4 = "As a mage you will have the following attributes:\nHealth: 75, If your health drops to 0 then you will die and have to restart that floor\nIQ: 25, This is the amount of damage each spell will be based on\nMana: 50, You have limited mana so pick your spells wisely, if you run out of mana mid fight you die\nSpecial ability called Soul drain"
-    mage5 = f"When you slect which spell you wish to use, 2 dices will be rolled\nIf the dices roll 2 - 6 then 50% of the IQ damage will be delt\nIf the dices roll a 7 - 11 then 90% of the IQ damage will be dealt\n If the dices roll a 12 then the special ability (Soul Drain) will be used and do the max damage possible"
-    mage6 = "To begin with you will recive 3 spells\n>Zap (Does the same amount of damage as your IQ attribute and takes 20 mana)\n>Fireball (Does IQ + 10 damage but takes 30 mana)\nEarthquake (Does IQ + 25 damage but takes 40 mana)"
-    mage7 = "At the end of each floor you may enter the shop and purchase better equipment using jerry coins"
-    mage8 = f"I hope you now understand how the dungeon works{name}, Now go and get that trophy!"
-
-def pickWarrior(name):
-    war1 = f"I see you like warriors{name}, good pick!"
-    war2 = "Now before we move forward let me explain how exactly the dungeons work"
-    war3 = "There are a total of 10 floors in this dungeon and the monsters will get harder each floor\nYou must survive all 10 floors and kill the Final Boss for the Trophy!"
-    war4 = "As a warrior you will have the following attributes:\nHealth: 100, If your health drops to 0 then you will die and have to restart that floor\nStrength: 35, This is the amount of damage each hit will do to the monsters\nSpecial ability called Enchanted Shuriken"
-    war5 = f"When you slect which move you wish to use, 2 dices will be rolled\nIf the dices roll 2 - 6 then you will lose 10% of your overlall health\nIf the dices roll a 7 - 11 then you lose 5% of your overall health\n If the dices roll a 12 then the special ability (Enchanted Shuriken) will be used and do the max damage possible"
-    war6 = f"To begin with you will recive 3 moves\nWeapon throw (Does the same amount of damage as your strength attribute)\nCharge (Does strength + 20 damage but you lose +5% of your overall health)\nBlood bath (Does strength + 30 damage but you lose +10% of your overall health)"
-    war7 = "At the end of each floor you may enter the shop and purchase better equipment using jerry coins"
-    war8 = f"I hope you now understand how the dungeon works{name}, Now go and get that trophy!"
+    if choice.lower() == "mage" or "m":
+        choiceMage(name)
+    elif choice.lower() == "w" or "warrior":
+        choiceWarrior(name)
+    else:
+        print("Fuck you")
 
 
-intro()
+choice()
